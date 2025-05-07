9042497f1d21c3ca9a7e374881bfc3ac1d562f39
GzFabri <faher93@gmail.com>
2025-05-07 11:55:28 -0500

    Se modifica Model de project

--- app/models/project.ts
+++ app/models/project.ts
@@ -5,6 +5,27 @@ export default class Proyecto extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
+  @column()
+  declare title: string
+
+  @column()
+  declare description: string
+
+  @column()
+  declare userId: number
+
+  @column()
+  declare statuId: number
+
+  @column()
+  declare clientId: number
+
+  @column()
+  declare startDate: string
+
+  @column()
+  declare endDate: string
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
