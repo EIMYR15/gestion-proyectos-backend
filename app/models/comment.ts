e987a69edfefb1a866e8534373b5710235c2dd1a
GzFabri <faher93@gmail.com>
2025-05-07 11:53:29 -0500

    Se modifica Model de Comment

--- app/models/comment.ts
+++ app/models/comment.ts
@@ -5,6 +5,15 @@ export default class Comentario extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
+  @column()
+  declare userId: number
+
+  @column()
+  declare taskId: number
+
+  @column()
+  declare description: string
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
